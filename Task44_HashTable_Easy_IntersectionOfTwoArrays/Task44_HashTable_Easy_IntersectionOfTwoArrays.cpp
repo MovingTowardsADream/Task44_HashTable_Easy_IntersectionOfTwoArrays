65143845697824829a4334fb81e1fd6343091d60
Matvey <zxdfcvas4554@mail.ru>
2023-07-26 19:05:54 +0300

    Add new solution

--- Task44_HashTable_Easy_IntersectionOfTwoArrays/Task44_HashTable_Easy_IntersectionOfTwoArrays.cpp
+++ Task44_HashTable_Easy_IntersectionOfTwoArrays/Task44_HashTable_Easy_IntersectionOfTwoArrays.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 class Solution {
 public:
     static std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
+        std::sort(nums1.begin(), nums1.end());
+        std::sort(nums2.begin(), nums2.end());
+        int first = 0, second = 0, n = nums1.size(), m = nums2.size();
         std::vector<int> result;
-        std::unordered_map<int, int> tmpmap;
-        for (int i = 0;i < nums1.size();i++)
-            tmpmap[nums1[i]]++;
-        for (int i = 0;i < nums2.size();i++)
-            if (tmpmap[nums2[i]] > 0)
-                tmpmap[nums2[i]] = -1;
-        for (auto i : tmpmap)
-            if (i.second == -1)
-                result.push_back(i.first);
+        while (first < n && second < m) {
+            std::cout << n << " " << m << " " << first << " " << second << std::endl;
+            if (nums1[first] == nums2[second]) {
+                result.push_back(nums1[first]);
+                while (first < n - 1 && nums1[first] == nums1[first + 1]) first++;
+                first++;
+                while (second < m - 1 && nums2[second] == nums2[second + 1]) second++;
+                second++;
+            }
+            else if(nums1[first] < nums2[second]){
+                first++;
+            }
+            else {
+                second++;
+            }
+        }
         return result;
     }
 };
 
 int main()
 {
-    std::vector<int> nums1 = { 1,2,2,1 };
-    std::vector<int> nums2 = { 2, 2 };
+    std::vector<int> nums1 = { 4,9,5 };
+    std::vector<int> nums2 = { 9,4,9,8,4 };
     for (int i : Solution::intersection(nums1, nums2)) {
         std::cout << i << " ";
     }
